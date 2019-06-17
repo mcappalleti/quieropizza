043e828c79c0101e209ef67316aa18a197b72126
mcappalleti <josselinemari@hotmail.es>
2019-06-16 20:47:45 -0500

    Agregamos una lista de productos

---
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,40 @@
+﻿using QuieroPizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 6 quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza 4 Estaciones";
+
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza Jamón y Queso";
+
+            var listadeProoductos = new List<ProductoModel>();
+            listadeProoductos.Add(producto1);
+            listadeProoductos.Add(producto2);
+            listadeProoductos.Add(producto3);
+
+
+
+
+
+            return View(listadeProoductos);
+        }
+    }
+}
